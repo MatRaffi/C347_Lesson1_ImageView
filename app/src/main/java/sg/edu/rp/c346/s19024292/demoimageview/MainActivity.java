fe33de7f193f6644342b52f21dc8e3f5ee7452aa
19024292 <19024292@myrp.edu.sg>
2021-04-20 12:00:30 +0800

    aa

--- app/src/main/java/sg/edu/rp/c346/s19024292/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/s19024292/demoimageview/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.s19024292.demoimageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day2);
     }
+
 }
